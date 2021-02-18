fecbcd2b4faf534538d0c30ba8ef23880bf9c37f
Steven Doss <steven.doss42@gmail.com>
2021-02-17 23:49:59 -0600

    Added command to spawn in a weapon on the current ped.

--- MyResourceSandboxClient/Class1.cs
+++ MyResourceSandboxClient/Class1.cs
@@ -19,14 +19,73 @@ namespace MyResourceSandboxClient
         {
             if (GetCurrentResourceName() != resourceName) return;
 
-            RegisterCommand("car", new Action<int, List<object>, string>((source, args, raw) =>
+            //Spawn weapon on character
+            RegisterCommand("weapon", new Action<int, List<object>, string>( (source, args, raw) =>
+           {
+               var model = "WEAPON_MICROSMG";
+               if(args.Count > 0)
+               {
+                   model = args[0].ToString();
+               }
+
+               var hash = (uint)GetHashKey(model);
+               //var hash = (uint)WeaponHash.MicroSMG;
+               if(!IsWeaponValid(hash))
+               {
+                   TriggerEvent("chat:addMessage", new
+                   {
+                       color = new[] { 255, 0, 0 },
+                       args = new[] { "[WeaponSpawner]", $"This is a invalid weapon, bro!" }
+                   });
+                   return;
+               }
+
+               //Give the player the weapon
+               GiveWeaponToPed( PlayerPedId(), hash, 20, false, true);
+
+               TriggerEvent("chat:addMessage", new
+               {
+                   color = new[] { 255, 0, 0 },
+                   args = new[] { "[CarSpawner]", $"Enjoy your new weapon! :)" }
+               });
+           }),false);
+
+            //Spawn car and enter the vehicle
+            RegisterCommand("car", new Action<int, List<object>, string>( async(source, args, raw) =>
             {
-                // TODO: make a vehicle! fun!
+                var model = "adder";
+                if(args.Count > 0)
+                {
+                    model = args[0].ToString();
+                }
+
+                var hash = (uint)GetHashKey(model);
+                if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
+                {
+
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        args = new[] { "[CarSpawner]", $"I wish I could spawn this {(args.Count > 0 ? $"{args[0]} or" : "")} adder but my owner was too lazy. :(" }
+                    });
+                    return;
+                }
+
+
+                // create the vehicle
+                var vehicle = await World.CreateVehicle(model, Game.PlayerPed.Position, Game.PlayerPed.Heading);
+
+                //Set the player ped into the vehicle and driver seat
+                Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
+
+                //Tell the player
+
                 TriggerEvent("chat:addMessage", new
                 {
                     color = new[] { 255, 0, 0 },
-                    args = new[] { "[CarSpawner]", $"I wish I could spawn this {(args.Count > 0 ? $"{args[0]} or" : "")} adder but my owner was too lazy. :(" }
+                    args = new[] { "[CarSpawner", $"Woohoo! Enjoy your new ^*{model}!" }
                 });
+
             }), false);
         }
     }
